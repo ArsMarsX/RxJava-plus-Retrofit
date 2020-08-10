04c206bfa845f9b3d22bb57b972fa9a58c7a027c
ArsMars <tag_mahal@mail.ru>
2020-08-10 19:00:27 +0300

    complete final version :ocean:

--- app/src/main/java/com/revolve44/rxjavaretrofit/MainActivity.kt
+++ app/src/main/java/com/revolve44/rxjavaretrofit/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.revolve44.rxjavaretrofit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
-import android.view.View
 import android.widget.Toast
-import com.revolve44.rxjavaretrofit.network.PopularMovies
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.revolve44.rxjavaretrofit.network.ServiceBuilder
+import com.revolve44.rxjavaretrofit.network.SuperResponse
 import com.revolve44.rxjavaretrofit.util.api_key
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import com.revolve44.rxjavaretrofit.network.Result
+import okhttp3.internal.http2.ErrorCode
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,38 +22,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        apilaunch()
+        Single.fromCallable {
+            lol()
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+
+    }
+
+    private fun apilaunch(){
         val compositeDisposable = CompositeDisposable() //?
         compositeDisposable.add(
-                ServiceBuilder.buildService().getMovies(api_key)
+            ServiceBuilder.buildService().getWeather("55.751244","37.618423", api_key)
 
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe({response ->
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                        response -> onResponse(response)
 
-                            onResponse(response)
+                }, {
+                        t -> onFailure(t)
+                    Log.e("ERROR", " ~ "+t)
+                }))
 
-                        }, {
-                            t -> onFailure(t)
-                        }))
+    }
 
+    private fun onResponse(response: SuperResponse) {
 
+        //var a =  response
+
+        Log.d("respone is ", " "+ Gson().toJson(response))
+        Log.d("TAG", "onResponse: ConfigurationListener::"+response.id);
+        Log.d("Threads num ", " respons is "+Thread.currentThread() + " UI is " + (Thread.currentThread() == Looper.getMainLooper().thread) )
+        //Log.d("respone is ", " "+ respon)
     }
 
     private fun onFailure(t: Throwable) {
+        //  HTTP 401 Unauthorized // retrofit2.adapter.rxjava2.HttpException: HTTP 401
         Toast.makeText(this,t.message, Toast.LENGTH_SHORT).show()
-        Log.d("ERROR ", " "+t.message)
-    }
-
-    private fun onResponse(response: PopularMovies) {
-
-        Log.d("respone is ", " "+response)
-        Log.d("Threads num ", " respons is "+Thread.currentThread() + " UI is " + (Thread.currentThread() == Looper.getMainLooper().thread) )
+        Log.d("ERROR ", " "+t.message+" // " +t)
 
     }
+
+
 
     private fun lol(){
-        var movie: Result
-
-        //Log.d(" movie is ", " $movie")
+        var a = 0
+        for (i in 1..100){
+            Thread.sleep(1000)
+            a++
+            Log.d("OPPSy", "try = " + a  +" thread "+Thread.currentThread())
+//            if (a<2){
+//
+//            }
+        }
     }
 }
--- app/src/main/java/com/revolve44/rxjavaretrofit/util/Contants.kt
+++ app/src/main/java/com/revolve44/rxjavaretrofit/util/Contants.kt
@@ -1,5 +1,7 @@
 package com.revolve44.rxjavaretrofit.util
 
-const val api_key="cfacbd1b17a84295a04a55d573daa740"
+//https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22
 
-const val base_url="https://api.themoviedb.org/"
+const val api_key="4e78fa71de97f97aa376e42f2f1c99cf"
+
+const val base_url="https://api.openweathermap.org/"
